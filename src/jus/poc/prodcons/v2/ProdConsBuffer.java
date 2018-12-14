3f897159a2f7394afae6fbe65c4175da98e4976b
remiv <remi.varenne@etu.univ-grenoble-alpes.fr>
2018-12-14 09:53:14 +0100

    Création des packages v2 v3

---
+++ src/jus/poc/prodcons/v2/ProdConsBuffer.java
@@ -0,0 +1,48 @@
+package jus.poc.prodcons.v2;
+
+import java.util.ArrayList;
+
+public class ProdConsBuffer implements IProdConsBuffer{
+
+  int nbreMess;
+  int sizeBuf;
+  int prodTime;
+  int consTime;
+  ArrayList<Message> buffer;
+  int consoCompte;
+
+  ProdConsBuffer (int sizeB) {
+    this.sizeBuf = sizeB;
+    buffer = new ArrayList<Message>(sizeB);
+    consoCompte = 0;
+    System.out.println(" *** ConsBuffer size "+sizeBuf + " *** ");
+  }
+
+  public synchronized void put(Message m) throws InterruptedException {
+    System.out.println(" + Producteur ID: " + m.prod.getId() + " put "+ m.message);
+    while(nmsg() == sizeBuf ) {
+      System.out.println(" ++ Prod en attente ! Buffer contient " + buffer.size() + " sur " + sizeBuf + " messages");
+      this.wait();
+    }
+    buffer.add(m);
+    System.out.println(" ++ Ajout ; buffer contient " + buffer.size() + " sur " + sizeBuf + " messages");
+    this.notifyAll();
+  }
+
+  public synchronized Message get() throws InterruptedException {
+    while(nmsg() == 0) {
+      System.out.println(" -- Conso en attente ! Consommés " + consoCompte);
+      this.wait();
+    }
+    Message m = buffer.remove(0);
+    System.out.println(" -- Nombre de message consommé : " + (consoCompte+1) + " ; buffer contient " + buffer.size() + " sur " + sizeBuf + " messages");
+    this.notifyAll();
+    return m;
+  }
+
+  @Override
+  public int nmsg() {
+    // TODO Auto-generated method stub
+    return buffer.size();
+  }
+}
---
+++ src/jus/poc/prodcons/v2/TestProdCons.java
@@ -0,0 +1,84 @@
+package jus.poc.prodcons.v2;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Random;
+
+public class TestProdCons extends Thread{
+
+
+  Properties properties;
+  int nbP;
+  int nbC;
+  int BufSz;
+  int ProdTime;
+  int ConsTime;
+  int Mavg;
+  int nbMessACreeTotal;
+  ArrayList<Producteur> producteurs;
+  Random random;
+  
+  TestProdCons(){
+    properties = new Properties();
+    producteurs = new ArrayList<Producteur>();
+    random = new Random();
+  }
+
+  public void start() {
+    try {
+      properties.loadFromXML(TestProdCons.class.getClassLoader().getResourceAsStream("jus/poc/prodcons/options.xml"));
+      nbP = Integer.parseInt(properties.getProperty("nbP"));
+      nbC = Integer.parseInt(properties.getProperty("nbC"));
+      BufSz = Integer.parseInt(properties.getProperty("BufSz"));
+      ProdTime = Integer.parseInt(properties.getProperty("ProdTime"));
+      ConsTime = Integer.parseInt(properties.getProperty("ConsTime"));
+      Mavg = Integer.parseInt(properties.getProperty("Mavg"));
+    }catch(InvalidPropertiesFormatException e) {
+      e.printStackTrace(); 
+    }catch(IOException e) {
+      e.printStackTrace();
+    }
+    ProdConsBuffer buffer = new ProdConsBuffer(BufSz);
+
+    int nbProd = 0;
+    int nbConso = 0;
+    nbMessACreeTotal = 0;
+
+    for(int i=0; i< (nbP + nbC);i++) {
+      if((nbProd != nbP) && (Math.random() < 0.5F)) {
+        Producteur p = new Producteur(ProdTime, (int) (random.nextGaussian() + Mavg), buffer);
+        producteurs.add(p);
+        nbMessACreeTotal += p.nbreMess;
+        nbProd += 1;
+        p.start();
+      }else if(nbConso != nbC){
+        Consommateur c = new Consommateur(ConsTime,buffer);
+        nbConso += 1;
+        c.setDaemon(true);
+        c.start();
+      }
+    }
+    System.out.println(" *** RECAP : " + nbP + " producteurs ; "+nbC+" consommateurs ; "+nbMessACreeTotal+" messages à créer ; "+ " buffer de taille "+ BufSz);
+    
+    producteurs.forEach(p -> {
+      try {
+        p.join();
+      } catch (InterruptedException e) {}
+    });
+    
+    while(buffer.consoCompte != nbMessACreeTotal) {
+      try {
+        this.wait();
+      } catch (InterruptedException e) {
+      }
+    }
+    System.out.println(" *** TERMINATE ***");
+  }
+
+  public static void main(String[] args){
+    TestProdCons test = new TestProdCons();
+    test.start();
+  }
+}
